4e89cabb9eebbb1b16ac7e0d4e42a846f40d1b71
saifulgmtl70 <saifulazad7061@gmail.com>
2023-11-04 20:28:41 +0600

    Update Signup.jsx

--- foodbevrage_Client/src/Components/SignUp/Signup.jsx
+++ foodbevrage_Client/src/Components/SignUp/Signup.jsx
@@ -83,7 +83,7 @@ const Signup = () => {
             const createdAt = result.user?.metadata?.creationTime;
             const user  = {  email, createdAt: createdAt}
 
-            fetch('https://food-bevrage-server.vercel.app//user',{
+            fetch('https://food-bevrage-server.vercel.app/user',{
                 method: 'POST',
                 headers: {
                     'content-type': 'application/json'
